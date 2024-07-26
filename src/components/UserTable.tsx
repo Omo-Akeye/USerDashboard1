28fb239ea7b768f95fef2fc89d940a01ff41ff87
Akeye <akeyesaheed@gmail.com>
2024-07-26 17:22:53 +0100

    updated table

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -9,6 +9,12 @@ export default function UserTable() {
       <h1 className='mt-6 mb-2 text-2xl font-bold'>Users & Roles</h1>
       <p className='text-[#667185]'>Manage all users in your business</p>
 
+
+      <div className='flex'>
+        <h1>Users</h1>
+        <h2>Roles</h2>
+      </div>
+
       <MainTable/>
     </div>
   )
